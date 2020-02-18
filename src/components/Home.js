6d29911ec8b068eef18987af8782d6c424c2fb5f
Anna Kovneva <kovnevaa@yahoo.com>
2020-02-18 14:30:03 -0800

    creating parallax + creatiing home page slider

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import {Parallax, ParallaxLayer} from 'react-spring/renderprops-addons'
+import AwesomeSlider from 'react-awesome-slider';
+import withAutoplay from 'react-awesome-slider/dist/autoplay';
+import 'react-awesome-slider/dist/styles.css';
+
 
 const home = () => {
+    let parallax;
     return (
-      <div className="content">
-       <h1>Home Page</h1>
-        <p>Home information page</p>
-    </div>
+        <Parallax pages={2} scrolling= {true} vertical ref={ref => parallax = ref}>
+            <ParallaxLayer offset={0.3} speed={0}>
+                <span>{slider}</span>
+            </ParallaxLayer>
+        </Parallax>
     );
 }
+
+const AutoplaySlider = withAutoplay(AwesomeSlider);
+ 
+const slider = (
+  <AutoplaySlider
+    play={true}
+    cancelOnInteraction={false} // should stop playing on user interaction
+    interval={6000}>
+    <div><img style = {{height: '100%', width: '100%'}} src ={require('../assets/lotsOfPlastic.jpg')} ></img></div>
+    <div><img style = {{height: '100%', width: '100%'}} src={require('../assets/platicBottle.jpg')}/></div>
+  </AutoplaySlider>
+);
+
 
 export default home;
