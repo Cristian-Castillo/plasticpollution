abfd126b7421e5940711fc3ed9bc85f2f3efb132
Anna Kovneva <kovnevaa@yahoo.com>
2020-02-18 18:10:18 -0800

    graph info graphic- homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,9 +8,16 @@ import 'react-awesome-slider/dist/styles.css';
 const home = () => {
     let parallax;
     return (
-        <Parallax pages={2} scrolling= {true} vertical ref={ref => parallax = ref}>
-            <ParallaxLayer offset={0.3} speed={0}>
+        <Parallax pages={4} scrolling= {true} vertical ref={ref => parallax = ref} style= {{background: '#4f4f4f'}} >
+            <ParallaxLayer offset={0} speed={0}>
                 <span>{slider}</span>
+            </ParallaxLayer>
+            <ParallaxLayer offset={1.2} speed= {0.5}>
+                <div style={{position: 'absolute', width: '100%'}}>
+                <h1 style= {{fontSize: 50, fontFamily: 'Orbitron', color: 'white' }}> We produce about 300 million tons of plastic per year</h1>
+                <iframe src="https://ourworldindata.org/grapher/global-plastics-production" style={{ margin: 50, width: '90%', height: 600, border: '0 none'}}></iframe>
+                </div>
+
             </ParallaxLayer>
         </Parallax>
     );
@@ -23,8 +30,14 @@ const slider = (
     play={true}
     cancelOnInteraction={false} // should stop playing on user interaction
     interval={6000}>
-    <div><img style = {{height: '100%', width: '100%'}} src ={require('../assets/lotsOfPlastic.jpg')} ></img></div>
-    <div><img style = {{height: '100%', width: '100%'}} src={require('../assets/platicBottle.jpg')}/></div>
+    <div>
+        <img style = {{height: '100%', width: '100%', position: 'relative'}} src ={require('../assets/lotsOfPlastic.jpg')} ></img>
+        <h1 style= {{ fontSize: 100, color: 'white', fontFamily: 'Orbitron', position:'absolute', width: '100%', top: 200, textAlign: 'center'}}>In a world full of plastic</h1>
+    </div>
+    <div>
+        <img style = {{height: '100%', width: '100%'}} src={require('../assets/platicBottle.jpg')}/>
+        <h1 style= {{ fontSize: 100, color: 'white', fontFamily: 'Orbitron', position:'absolute', width: '100%', top: 200, textAlign: 'center'}}>What's YOUR impact?</h1>
+        </div>
   </AutoplaySlider>
 );
 
