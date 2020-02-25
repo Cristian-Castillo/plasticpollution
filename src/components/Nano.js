d50d714cc5355acfdbf139894b3f3d2eca86e4c4
Cristian Carrillo Castillo Jr <ccarri11@ucsc.com>
2020-02-25 00:06:32 -0800

    added Nano parallax image, content, and button to Nano Page

--- src/components/Nano.js
+++ src/components/Nano.js
@@ -11,24 +11,36 @@ import {Trail} from 'react-spring/renderprops'
 const Nano = () => {
 
 
-    return (
-       <Spring from ={{ opacity: 0, marginTop: -500}}
-               to ={{ opacity: 1, marginTop:0 }}>
-        {props => (
+  return (
+     <Spring from ={{ opacity: 0, marginTop: -500}}
+             to ={{ opacity: 1, marginTop:0 }}>
+      {props => (
 
-        <div style= {props}>
-          <div className="backgroundNano parallax">
-          </div>
-          <div className="backgroundNano parallax">
-          </div>
-          <NanoComponentTwo />
-          <div>
-
-          </div>
+      <div style= {props}>
+        <div className="backgroundNano parallax">
         </div>
-        )}
-       </Spring>
-    );
+        <div className="backgroundNano parallax">
+        </div>
+        <NanoComponentTwo />
+        <div>
+
+        </div>
+        <div className="parallaxOrigin">
+        </div>
+        <button className="nanoButton"> Begin Journey </button>
+
+        <div className ="nanoInfo">
+        <h1> Nano Plastics</h1>
+        <p className ="passageOrigin">Plastic litters have become the predominant components of marine debris due to extensive consumption plastics and mismanagement of plastic wastes. As part of the problem, microplastics (MPs) and nanoplastics (NPs) have generated special concerns due to their unique features that make them easy to transfer among oceans in the marine ecosystem, across different trophic levels inside the food web, and even across different tissues inside contaminated animals. Studies have demonstrated the almost omnipresence of MPs in the marine ecosystem, which present serious threats to the health of marine animals, causing symptoms such as malnutrition, inflammation, chemical poisoning, growth thwarting, decrease of fecundity, and death due to damages at individual, organ, tissue, cell, and molecule levels. </p>
+        </div>
+
+
+
+      </div>
+      )}
+     </Spring>
+  );
+
 }
 
 
