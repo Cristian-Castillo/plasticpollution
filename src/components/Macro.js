8ef51630ad74c345ee6ff7204f1eac809c2433b1
Collin Hurst <collinhurst12@gmail.com>
2020-02-27 06:00:12 -0800

    not sure if it works

--- src/components/Macro.js
+++ src/components/Macro.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import bins from './assets/bins.jpg'
+import binstrash from './assets/binstrash.jpg'
+import binsdump from './assets/binsdump.jpg'
+import binsrecycle from './assets/binsrecycle.jpg'
+import binsbackpck from './assets/binsbackpack.jpg'
+import './Macro.css'
 
-const Macro = () => {
+var img = document.getElementById('scene');
+
+function hover(element) {
+
+  img.setAttribute('src', {binstrash});
+}
+
+function unhover(element) {
+  img.setAttribute('src', {bins});
+}
+
+const Macro = (myRef) => {
     return (
-      <div className="content">
+      <div className="scene_container">
          <h1>Macro Page</h1>
           <p>Macro information</p>
+          <img id="scene"  ref={this.myRef} src={bins} className="scene" onMouseOver={hover(this)} onMouseOut={unhover(this)} ></img>
       </div>
+
     );
 }
 
