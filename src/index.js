9ee2a216df082d52a9e7d252671c6a3ded7863cd
Cristian Carrillo Castillo Jr <ccarri11@ucsc.com>
2020-03-04 20:48:10 -0800

    update to routes

--- src/index.js
+++ src/index.js
@@ -4,42 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './fonts/Orbitron-Regular.ttf'
-import {HashRouter as Route} from 'react-router-dom'
+import {HashRouter as Router} from 'react-router-dom'
 import './App.css';
-import Navigation from './Navigation'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-import Error from './components/Error';
-import Faq from './components/Faq';
-import Adventure from './components/Adventure';
-import Activist from './components/Activist';
-import Macro from "./components/Macro";
-import Micro from "./components/Micro";
-import Nano from "./components/Nano";
-import HumanConsumptionNano from "./components/HumanConsumptionNano";
-import PreventionJourney from "./components/PreventionJourney";
-import NanoEvaporation from "./components/NanoEvaporation";
 
 ReactDOM.render(
-  <Route>
+  <Router>
   <App />
-   <Route path="/" component={Home} exact/>
-   <Route path="/about" component={About}/>
-   <Route path="/contact" component={Contact}/>
-   <Route path="/faq" component={Faq}/>
-   <Route path="/adventure" component={Adventure}/>
-   <Route path="/Macro" component={Macro}/>
-   <Route path="/Micro" component={Micro}/>
-   <Route path="/Nano" component={Nano}/>
-   <Route path="/activist" component={Activist}/>
-   <Route path="/HumanConsumptionNano" component={HumanConsumptionNano}/>
-   <Route path="/PreventionJourney" component={PreventionJourney}/>
-   <Route path="/NanoEvaporation" component={NanoEvaporation}/>
-   <Route path ="/Error" component={Error}/>
-  </Route>
+</Router>
   , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
