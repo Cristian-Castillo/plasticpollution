2991dcf93ca52c63df8fd581fc414078addb3768
Cristian Carrillo Castillo Jr <ccarri11@ucsc.com>
2020-03-04 20:04:39 -0800

    closer to making local & github work together in terms of link pages through site

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navigation from './Navigation'
-import {BrowserRouter,Switch,Route} from 'react-router-dom';
+import {BrowserRouter,Switch,Route,Link} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Home from './components/Home';
