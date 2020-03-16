cd8b150f7c30ff95e10061da921b7d7791f54f07
Anna Kovneva <kovnevaa@yahoo.com>
2020-03-15 17:17:31 -0700

    fixed UI for micro

--- src/components/Micro.js
+++ src/components/Micro.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import MicroCss from './Micro.css'
+import './Micro.css'
 import MicroComponentTwo from './MicroComponentTwo';
 import Nav from 'react-bootstrap/Nav';
 import { Spring } from 'react-spring/renderprops';
-import {Transition} from 'react-spring/renderprops'
-import {useTransition} from 'react-spring'
-import {useSprings, animated} from 'react-spring'
-import {Trail} from 'react-spring/renderprops'
 
 const Micro = () => {
     return (
@@ -14,35 +10,35 @@ const Micro = () => {
              to ={{ opacity: 1, marginTop:0 }}>
       {props => (
 
-      <div style= {props}>
-        <div className="backgroundMicro parallax">
+      <div  className="title_backgroundMicro" style= {props}>
+        <div className="backgroundMicro parallaxMicro">
         </div>
-        <div className="backgroundMicro parallax">
+        <div className="backgroundMicro parallaxMicro">
         </div>
         <div>
         <MicroComponentTwo/>
         </div>
 
-        <div className="bigCont">
-        <div className="oceanCont"> 
-          <div className="parallaxOriginM OceanImg"></div>
-          <div className ="microInfo oceanInfo">
+        <div className="bigContMicro">
+        <div className="oceanContMicro"> 
+          <div className= "OceanImgMicro"></div>
+          <div className ="microInfoMicro oceanInfoMicro">
           <h2> Micro Plastics in the Ocean</h2>
-          <p className ="passageOrigin">Lorem ipsum dolor sit amet, te mei
+          <p className ="passageOriginMicro">Lorem ipsum dolor sit amet, te mei
 
 In putant fabulas vix, in mea cibo nullam. Case vulputate nesarchum moderatius est. Sea ex quod vero. An munere numquam quaestio duo. Postea equidem gloriatur vis ne, ne harum essent forensibus usu, esse essent epicurei ne qui. </p>
-          <button className="microButton"><Nav.Link href="/Ocean">Dive into Adventure </Nav.Link></button>
+          <button className="microButtonMicro"><Nav.Link href="/Ocean">Dive into Adventure </Nav.Link></button>
           </div>
           </div>
 
-          <div className="oceanCont"> 
-          <div className="parallaxOriginM GrassImg"></div>
-          <div className ="microInfo oceanInfo">
+          <div className="oceanContMicro"> 
+          <div className="GrassImgMicro"></div>
+          <div className ="microInfoMicro oceanInfoMicro">
           <h2> Micro Plastics on Land</h2>
-          <p className ="passageOrigin">Lorem ipsum dolor sit amet, te mei petentium efficiendi, te platonem iudicabit sea. Affert accusamus dissentiunt cu vel, ex vel minim nominavi disputationi. Ex mel dolorum recteque, cu cum torquatos interesset. Tempor doctus explicari vis in. Fugit clita in vel, usu ne quas delenit. Ut eros duis qualisque nam, ei vel paulo civibus. Est nostro senserit intellegat et.
+          <p className ="passageOriginMicro">Lorem ipsum dolor sit amet, te mei petentium efficiendi, te platonem iudicabit
 
 In putant fabulas vix, in mea cibo nullam. Case vulputate no vel, eu aliquip utroque convenire duo, te veri mnesarchum moderatius est. Sea ex quod vero. An munere numquam quaestio duo. Postea equidem gloriatur vis ne, ne harum essent forensibus usu, esse essent epicurei ne qui. </p>
-          <button className="microButton"> Journey through Land </button>
+          <button className="microButtonMicro"><Nav.Link href="/Ocean">Journey through Land</Nav.Link> </button>
           </div>
           </div>
 
--- src/components/MicroComponentTwo.js
+++ src/components/MicroComponentTwo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import MicroCss from './Micro.css'
+import  './Micro.css'
 import {Spring } from 'react-spring/renderprops';
 
 const MicroComponentTwo = () => {
@@ -12,11 +12,11 @@ const MicroComponentTwo = () => {
         {props => (
           <div style= {props}>
 
-          <div className="backgroundMicroC2 parallax">
-            <h1 className ="colorText">Begin A Micro Adventure</h1>
+          <div className="backgroundMicroC2 parallaxMicro">
+            <h1 className ="colorTextMicro">Begin A Micro Adventure</h1>
           </div>
-          <div className ="paraDiv">
-          <p className ="introInfo">
+          <div className ="paraDivMicro">
+          <p className ="introInfoMicro">
           For human health, it is the smallest particles – micro- and nano-particles which are small enough to be ingested – that are of greatest concern. There are several ways by which plastic particles can be ingested: orally through water, consumption of marine products which contain microplastics, through the skin via cosmetics (identified as highly unlikely but possible), or inhalation of particles in the air.
           </p>
           </div>
