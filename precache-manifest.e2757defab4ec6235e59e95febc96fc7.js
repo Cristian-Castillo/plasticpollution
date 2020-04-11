2d128bf3f136e25873910fcdb3fada2454e63bba
Cristian Carrillo Castillo Jr <ccarri11@ucsc.com>
2020-04-11 11:52:57 -0700

    Updates

---
+++ precache-manifest.e2757defab4ec6235e59e95febc96fc7.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a781addd4e8f013f2ea3e627d7e1b08",
+    "url": "/plasticpollution/index.html"
+  },
+  {
+    "revision": "704dca2cecec5ee20906",
+    "url": "/plasticpollution/static/css/2.9df9867b.chunk.css"
+  },
+  {
+    "revision": "c02883ab2eccbaf53dbd",
+    "url": "/plasticpollution/static/css/main.8a25d83f.chunk.css"
+  },
+  {
+    "revision": "704dca2cecec5ee20906",
+    "url": "/plasticpollution/static/js/2.f31d7546.chunk.js"
+  },
+  {
+    "revision": "c8cad2fb08b129c7a998cdaf9bda8169",
+    "url": "/plasticpollution/static/js/2.f31d7546.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c02883ab2eccbaf53dbd",
+    "url": "/plasticpollution/static/js/main.254389e7.chunk.js"
+  },
+  {
+    "revision": "674042159570c5a8a734",
+    "url": "/plasticpollution/static/js/runtime-main.aa25dee3.js"
+  },
+  {
+    "revision": "add0c4244a015960586eeb51f091fd71",
+    "url": "/plasticpollution/static/media/Orbitron-Regular.add0c424.ttf"
+  },
+  {
+    "revision": "b4d358aa6ba16d1c57d8c920e46aa72d",
+    "url": "/plasticpollution/static/media/SammyTwo.b4d358aa.png"
+  },
+  {
+    "revision": "a86031dd20f35163486de6174e48cc18",
+    "url": "/plasticpollution/static/media/bins.a86031dd.jpg"
+  },
+  {
+    "revision": "1e9be2c7a18b4fbc93956607ae91f7ed",
+    "url": "/plasticpollution/static/media/binsbackpack.1e9be2c7.jpg"
+  },
+  {
+    "revision": "295c76f83d5d911d103c1d5f520ff184",
+    "url": "/plasticpollution/static/media/binsdump.295c76f8.jpg"
+  },
+  {
+    "revision": "2d9d2dcdb7df99ac7a3ef0e02a894052",
+    "url": "/plasticpollution/static/media/binsrecycle.2d9d2dcd.jpg"
+  },
+  {
+    "revision": "c2f94d0971608489658215789c4e1b55",
+    "url": "/plasticpollution/static/media/binstrash.c2f94d09.jpg"
+  },
+  {
+    "revision": "e62fc7bab980ee3172a5b2995c63a241",
+    "url": "/plasticpollution/static/media/chinashipping.e62fc7ba.jpg"
+  },
+  {
+    "revision": "31d76638902c369f51afad77fc56aa51",
+    "url": "/plasticpollution/static/media/kitchen.31d76638.jpg"
+  },
+  {
+    "revision": "2a0828f2d88cdc5edad86bf5b70afd58",
+    "url": "/plasticpollution/static/media/landfill.2a0828f2.jpg"
+  },
+  {
+    "revision": "681107c1e1085a4071f2526dc2f78053",
+    "url": "/plasticpollution/static/media/lotsOfPlastic.681107c1.jpg"
+  },
+  {
+    "revision": "8fcc7ac2d2c9fd6927b164a5f1e03f14",
+    "url": "/plasticpollution/static/media/macro.8fcc7ac2.jpg"
+  },
+  {
+    "revision": "1f0319e2fd4f5e163d3276efa397944a",
+    "url": "/plasticpollution/static/media/micro.1f0319e2.jpg"
+  },
+  {
+    "revision": "7f65cb4cf5931f6fc156139ee0ff3ab4",
+    "url": "/plasticpollution/static/media/micropieces.7f65cb4c.svg"
+  },
+  {
+    "revision": "1e3dc5c41e85380f69c7e3dc85487563",
+    "url": "/plasticpollution/static/media/nano.1e3dc5c4.jpg"
+  },
+  {
+    "revision": "c059c544a954729d1afd3e2355437ac3",
+    "url": "/plasticpollution/static/media/net.c059c544.png"
+  },
+  {
+    "revision": "5bcb1dd6718fb5514aeea757913a8c04",
+    "url": "/plasticpollution/static/media/ocean.5bcb1dd6.jpg"
+  },
+  {
+    "revision": "8d27566ac820c8250c53000cdc2f882c",
+    "url": "/plasticpollution/static/media/ocean.8d27566a.jpg"
+  },
+  {
+    "revision": "67fcffdb9455d5fbb4023d8dfa6665a2",
+    "url": "/plasticpollution/static/media/ocean_blue.67fcffdb.jpg"
+  },
+  {
+    "revision": "15fefea1a356bd6ac962fce42c182837",
+    "url": "/plasticpollution/static/media/plasticbag.15fefea1.png"
+  },
+  {
+    "revision": "ccfe6205dfedad177eeb423f6cd7f366",
+    "url": "/plasticpollution/static/media/platicBottle.ccfe6205.jpg"
+  },
+  {
+    "revision": "0c553c244183c34ff73a6a323a163fe8",
+    "url": "/plasticpollution/static/media/recyclingfactory.0c553c24.jpg"
+  },
+  {
+    "revision": "ac5c7a4ea0656f1bc010498ebefb92fd",
+    "url": "/plasticpollution/static/media/redcircle.ac5c7a4e.png"
+  },
+  {
+    "revision": "ea89d2d621f1b1da65beef7770dd0172",
+    "url": "/plasticpollution/static/media/sammy.ea89d2d6.png"
+  },
+  {
+    "revision": "39ae4476186d007b7a5b6ac1d9ef9daa",
+    "url": "/plasticpollution/static/media/stormdrain.39ae4476.jpg"
+  },
+  {
+    "revision": "c39c8394c70c92d96db3b5ce4c12f5ee",
+    "url": "/plasticpollution/static/media/turtle.c39c8394.svg"
+  },
+  {
+    "revision": "302aed30caa2a752d69f8b03d17c4180",
+    "url": "/plasticpollution/static/media/ucb.302aed30.png"
+  },
+  {
+    "revision": "80fb4a2d241ef5f475933c6ab1dffb62",
+    "url": "/plasticpollution/static/media/uci.80fb4a2d.png"
+  },
+  {
+    "revision": "6c9afce8e2e3849c6ee684c5f2bfa6a8",
+    "url": "/plasticpollution/static/media/ucla.6c9afce8.png"
+  },
+  {
+    "revision": "930420e534c95494ae88c861bf4c6bbf",
+    "url": "/plasticpollution/static/media/ucm.930420e5.png"
+  },
+  {
+    "revision": "8265ad5eeab2fc6ad05374afb873b21b",
+    "url": "/plasticpollution/static/media/ucr.8265ad5e.png"
+  },
+  {
+    "revision": "13ab20c285340c686ba910dbdcc85a81",
+    "url": "/plasticpollution/static/media/ucsc.13ab20c2.png"
+  },
+  {
+    "revision": "825a0f8335f07d783f4b95fa8030ca4a",
+    "url": "/plasticpollution/static/media/wave (3).825a0f83.svg"
+  }
+]);
