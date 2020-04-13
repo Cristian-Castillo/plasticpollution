df434a20ec01de115ea203f503f19edd1acef2ab
cris-c-castillo <ccarri11@ucsc.com>
2020-04-12 20:33:27 -0700

    update

--- nextjs/pages/_app.js
+++ nextjs/pages/_app.js
@@ -1,30 +1,5 @@
 import '../styles.css'
-import AboutPg from '../About.css'
 import Home from '../Home.css'
-import '../turtleAnim.css'
-import turtleCSS from '../turtle.css'
-import '../microOcean.css'
-import '../react-awesome-slider/dist/styles.css';
-import FishCSS from '../Fish.css';
-import SunCloud from '../sunCloud.css';
-import BlobFish from '../NanoBlobFish.css';
-import HumanConsumptionNanoCSS from '../HumanConsumptionNano.css';
-import NanoEvaporationCSS from './NanoEvaporation.css';
-import '../Macro.css'
-import NanoCss from '../Nano.css'
-import '../bootstrap/dist/css/bootstrap.min.css';
-import '../Macro2.css'
-import sushi from '../Sushi.css';
-import '../MacroAsia.css'
-import '../microOcean.css'
-import '../MacroDump.css'
-import '../MacroOcean.css'
-import '../MacroRecycle.css'
-import '../MacroReuse.css'
-import '../Micro.css'
-import '../microOcean.css'
-import '../microOcean.css'
-import MicroOceanCss from '../microOcean.css'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
